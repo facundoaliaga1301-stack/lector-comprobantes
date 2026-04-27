2453520f2f746b88ef541b785e5450abcd04bcd4
Facundo <facundo.aliaga1301@gmail.com>
2026-04-27 11:22:56 -0300

    fix port railway

--- app.py
+++ app.py
@@ -268,4 +268,4 @@ def index():
     return render_template("index.html", results=results, banco=banco, operacion=operacion)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
